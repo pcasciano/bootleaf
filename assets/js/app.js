47b587536a18070fa1552be738e42ac5983522f4
pas <pa.casciano@alice.it>
2014-11-14 16:04:50 +0100

    getfeatureinfo support added

--- assets/js/app.js
+++ assets/js/app.js
@@ -60,6 +60,7 @@ overlayLayers[qs.layer]=layer;
 map = L.map("map", {
   zoom: 10,
   center: [cy, cx],
+  crs: L.CRS.EPSG4326,
   layers: [layer, mapquestOSM],
   zoomControl: false,
   attributionControl: false
@@ -80,6 +81,75 @@ var layerControl = L.control.groupedLayers(baseLayers, {
 
 L.control.layers(baseLayers, overlayLayers).addTo(map);
 
+// -- Display information on click --
 
+// Add an event handler for the map "click" event
+map.on('click', function(e) {
+
+    // Build the URL for a GetFeatureInfo
+    var url = getFeatureInfoUrl(
+                    map,
+                    layer,
+                    e.latlng,
+                    {
+                        'info_format': 'application/json',
+                        'propertyName': 'NAME,AREA_CODE,DESCRIPTIO'
+                    }
+                );
+
+    // Send the request and create a popup showing the response
+    reqwest({
+        url: url,
+        type: 'json',
+    }).then(function (data) {
+        var feature = data.features[0];
+        L.popup()
+        .setLatLng(e.latlng)
+        .setContent(L.Util.template("<h2>{NAME}</h2><p>{DESCRIPTIO}</p>", feature.properties))
+        .openOn(map);
+    });
+
+});
+
+/**
+ * Return the WMS GetFeatureInfo URL for the passed map, layer and coordinate.
+ * Specific parameters can be passed as params which will override the
+ * calculated parameters of the same name.
+ */
+function getFeatureInfoUrl(map, layer, latlng, params) {
+
+    var point = map.latLngToContainerPoint(latlng, map.getZoom()),
+        size = map.getSize(),
+        bounds = map.getBounds();
+        sw = bounds.getSouthWest(),
+        ne = bounds.getNorthEast();
+       /* sw = crs.projection._proj.forward([sw.lng, sw.lat]),
+        ne = crs.projection._proj.forward([ne.lng, ne.lat]);*/
+
+
+    var defaultParams = {
+        request: 'GetFeatureInfo',
+        service: 'WMS',
+        srs: layer._crs.code,
+        styles: '',
+        version: layer._wmsVersion,
+        format: layer.options.format,
+        //bbox: [sw.join(','), ne.join(',')].join(','),
+	bbox:sw.lat+','+sw.lng+','+ne.lat+','+ne.lng,
+        height: size.y,
+        width: size.x,
+        layers: layer.options.layers,
+        query_layers: layer.options.layers,
+        info_format: 'text/html'
+    };
+
+    params = L.Util.extend(defaultParams, params || {});
+
+    params[params.version === '1.3.0' ? 'i' : 'x'] = point.x;
+    params[params.version === '1.3.0' ? 'j' : 'y'] = point.y;
+
+    return layer._url + L.Util.getParamString(params, layer._url, true);
+
+}
 
 
